43a2d74548ab125861bc1906646d3b002d08dd2a
togor <2851045505@qq.com>
2016-12-22 16:43:10 +0800

    提交第一版zAudiojs

---
+++ README.md
@@ -0,0 +1,9 @@
+This JS replaces the native audio style,and provides different effect. In the same page limit can only one audio file.
+
+you only need to add a data-audio="z-audio" above the original audio tags.
+
+If you want to display a audio control in a circular way, you can set data-style="circle".
+
+The plugin supports style extensions,you can customize CSS styles,you just need set date-style="example" and  modify CSS file to add zAudio-example class.
+
+In zAudio.css file reference Font Icon,you can replace the font icon for youself.
---
+++ z-audio.js
@@ -0,0 +1,185 @@
+/**
+ *	author:   togor
+ *	time:     2016-12-21
+ *	describe: zAudio plug js
+ *  version:  1.0
+ **/
+
+window.onload = function(){
+
+	//var au = $('body').find('audio[data-audio="z-audio"]');
+	var au = document.getElementsByTagName('audio'),
+		auArray = [];
+	for (var i = 0; i < au.length; i++) {
+		if (au[i].attributes['data-audio'].value == 'z-audio') {
+			auArray.push(au[i]);
+		}
+	}
+	for(var i = 0; i < auArray.length; i++){
+		var src = auArray[i].attributes['src'].value;
+		zAudio.call(auArray[i]);
+	}
+
+	function zAudio(){
+		var _this = this,
+			src = _this.attributes['src'].value;
+		
+		
+		_this.appear = function(){
+			/* add audio controls used to display nodes.*/
+			_this.setAttribute('controls','controls');
+
+			var audioStyle = _this.attributes['data-style'] ? _this.attributes['data-style'].value : 'default';
+			var audioBox = document.createElement('div');
+
+			/* Detemine whether the node has src attributes,if not and give a response prompt. */
+			if (src) { 
+				var button = document.createElement('span');
+				var timeLine = document.createElement('div');
+				var overLine = document.createElement('span');
+				var point = document.createElement('i');
+				var audioTime = document.createElement('span');
+				var audio = _this.cloneNode(true);
+
+				audioBox.setAttribute('class','zAudio zAudio-'+audioStyle);
+				button.setAttribute('class','zAudio-button z-play disabled');
+				timeLine.setAttribute('class','timeLine');
+				overLine.setAttribute('class','overLine');
+				audioTime.setAttribute('class','audioTime z-load');
+				point.setAttribute('class','point');
+
+				audioBox.appendChild(button);
+				overLine.appendChild(point);
+				timeLine.appendChild(overLine);
+				audioBox.appendChild(timeLine);
+				audioBox.appendChild(audioTime);
+				audioBox.appendChild(audio);
+
+				_this.parentNode.insertBefore(audioBox,_this);
+				
+				/* Dynamically set the width of the timeline. */
+				timeLine.style.width = audioBox.offsetWidth - button.offsetWidth - audioTime.offsetWidth - 70 + 'px';
+				timeLine.style.left = button.offsetWidth + 36 + 'px';
+
+				if (audioStyle == 'circle') {
+					button.style.lineHeight = audioBox.offsetHeight + 'px';
+					button.style.fontSize = audioBox.offsetHeight/4 + 'px';
+				}
+
+				/* Set timer to get audio length, */
+				var duration,seter;
+				var s = setInterval(function(){
+					console.log(audio)
+					if (isNaN(audio.duration)) {
+						return false;
+					}else{
+						clearInterval(s);
+						/* Dynamically set the audioTime and set to can play state. */
+						duration = audio.duration;
+						audioTime.innerHTML = formatTime(duration);
+						removeCla(button,'disabled');
+						removeCla(audioTime,'z-load');
+					}
+				},1000);
+
+				/* bind event */
+				bindEvent(button,'click',function(){
+					if (hasCla(button,'disabled')) {
+						return false;
+					}else if (hasCla(button,'z-play')) {
+						/* play audio */
+						audio.play();
+						removeCla(button,'z-play');
+						addCla(button,'z-stop');
+						console.log(document.getElementsByTagName('audio'));
+						var allAudio = document.getElementsByTagName('audio');
+						for (var i = 0; i < allAudio.length; i++) {
+							allAudio[i].setAttribute('data-playStatus','false');
+						}
+						audio.setAttribute('data-playStatus','true');
+						/* Dynamic display audio remaining time. */
+						seter = setInterval(function(){
+							audioTime.innerHTML = formatTime(duration- audio.currentTime);
+							overLine.style.width = (audio.currentTime/duration) * 100 + '%';
+							if (duration - audio.currentTime <= 0 || audio.getAttribute('data-playStatus') == 'false') {
+								clearInterval(seter);
+								removeCla(button,'z-stop');
+								addCla(button,'z-play');
+								audio.pause();
+							}
+							console.log(duration- audio.currentTime)
+						},100);
+
+					}else if(hasCla(button,'z-stop')){
+
+						/* clear interval. */
+						clearInterval(seter);
+
+						audio.pause();
+						audioTime.innerHTML = formatTime(duration- audio.currentTime);
+						removeCla(button,'z-stop');
+						addCla(button,'z-play');
+						audio.setAttribute('data-playStatus','false');
+					}
+				});
+			}else{
+				var tips = document.createElement('p');
+				tips.setAttribute('class','error-tips')
+				tips.innerHTML = 'this audio not have src.';
+				audioBox.appendChild(tips);
+				_this.parentNode.insertBefore(audioBox,_this);
+			}
+			
+			/* remove the original audio node. */
+			_this.parentNode.removeChild(_this);
+		};
+		
+		_this.appear();
+	}
+	
+	function formatTime(time){
+		time = Math.ceil(time);
+		var h = Math.floor(time/3600);
+		var m = Math.floor((time%3600)/60);
+		var s = (time%3600)%60;
+		function addZero(obj){
+			if (obj<10) {
+				return '0'+obj;
+			}else{
+				return obj;
+			}
+		}
+		if (h<=0) {
+			time = addZero(m) + ':' + addZero(s);
+		}else{
+			time = addZero(h) + ':' + addZero(m) + ':' + addZero(s);
+		}
+		return time;
+	}
+	function removeCla(obj,cla){
+		var _cla = obj.getAttribute('class');
+		_cla = _cla.replace(cla,'');
+		_cla = _cla.replace('  ',' ');
+		obj.setAttribute('class',_cla);
+	}
+	function hasCla(obj,cla){
+		if (obj.getAttribute('class').indexOf(cla)>=0) {
+			return true;
+		}else{
+			return false;
+		}
+	}
+	function addCla(obj,cla){
+		var _cla = obj.getAttribute('class');
+		if (_cla.charAt(_cla.length-1) == ' ') {
+			_cla = _cla + cla;
+		}else{
+			_cla = _cla + ' ' + cla;
+		}
+		obj.setAttribute('class',_cla);
+	}
+	function bindEvent(obj,type,fn){
+		if(obj.attachEvent){obj.attachEvent('on'+type,fn);}
+		else{obj.addEventListener(type,fn,false)}
+	}
+}
